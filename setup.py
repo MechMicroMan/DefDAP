3f25c4506cb3738e76aeed09c42f3fa598e38f15
Rhys Thomas <rhys.thomas@manchester.ac.uk>
2020-05-06 15:22:20 +0100

    Require matplotlib version 3+

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='DefDAP',
     version='0.92',
-    description='A package for combined analysis of EBSD and DIC data.',
+    description='A python library for correlating EBSD and HRDIC data.',
     author='Michael D. Atkinson',
     packages=find_packages(),
     install_requires=[
         'scipy',
         'numpy',
-        'matplotlib',
+        'matplotlib>=3.0.0',
         'scikit-image',
         'scikit-learn',
         'pandas',
