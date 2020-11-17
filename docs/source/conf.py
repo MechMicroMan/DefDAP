bb6e915825e2afeea84f9a218f382f2081218827
Rhys Thomas <rhys.thomas@manchester.ac.uk>
2020-11-17 18:04:01 +0000

    Add requirements for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'modules/defdap.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'defdap/defdap.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -144,7 +144,7 @@ def run_apidoc(_):
             '--no-toc',                     # No table of contents
             '../../defdap',                 # Module path
             '-o',                           # Directory to output..
-            '../source/defdap'             # here
+            '../source/defdap'              # here
             ]
 
     # Invoke apidoc
