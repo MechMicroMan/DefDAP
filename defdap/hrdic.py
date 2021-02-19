a17d368b8919931ab4bdf98290d40f7c59e1d1a0
Rhys Thomas <rhys.thomas@manchester.ac.uk>
2021-02-19 17:12:43 +0000

    Transform boundary lines to DIC reference

--- defdap/hrdic.py
+++ defdap/hrdic.py
@@ -488,6 +488,10 @@ class Map(base.Map):
             self.ebsdTransform = tf.AffineTransform()
             self.ebsdTransformInv = self.ebsdTransform.inverse
 
+        # Also transform the EBSD boundaryLines to DIC reference frame
+        boundaryLineList = np.array(self.ebsdMap.boundaryLines).reshape(-1,2)               #flatten to coord list
+        self.boundaryLines = TDirr[2].ebsdTransformInv(boundaryLineList).reshape(-1,2,2)    #reshape back to coord pairs
+
         # calculate transform from EBSD to DIC frame
         self.ebsdTransform.estimate(
             np.array(self.homogPoints),
