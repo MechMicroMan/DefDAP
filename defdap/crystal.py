f871642fdf757010b4b6a160fa5bdaae8dbd4972
Rhys Thomas <rhys.thomas@manchester.ac.uk>
2021-03-06 17:17:09 +0000

    Update copyright

--- defdap/crystal.py
+++ defdap/crystal.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Mechanics of Microstructures Group
+# Copyright 2021 Mechanics of Microstructures Group
 #    at The University of Manchester
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
