9afac1530cca66dddf36c5ada442f0d4d4c36e17
Michael Atkinson <michael.atkinson@manchester.ac.uk>
2022-03-07 17:27:32 +0000

    Update oxford binary loader
    Assume unknown data fields are float 32

--- defdap/file_readers.py
+++ defdap/file_readers.py
@@ -326,6 +326,7 @@ class OxfordBinaryLoader(EBSDDataLoader):
         self.checkMetadata()
 
         # Construct binary data format from listed fields
+        unknown_field_count = 0
         dataFormat = [('phase', 'uint8')]
         fieldLookup = {
             3: ('ph1', 'float32'),
@@ -344,7 +345,10 @@ class OxfordBinaryLoader(EBSDDataLoader):
                 fieldID = int(metadata['Fields']['Field{:}'.format(i + 1)])
                 dataFormat.append(fieldLookup[fieldID])
         except KeyError:
-            raise TypeError("Unknown data in EBSD file.")
+            print(f'\nUnknown field in file with key {fieldID}. '
+                  f'Assumming float32 data.')
+            unknown_field_count += 1
+            dataFormat.append((f'unknown_{unknown_field_count}', 'float32'))
 
         self.dataFormat = np.dtype(dataFormat)
 
