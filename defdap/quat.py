111ee7314bde88b7227435957a99fb0489fc7bd9
Michael Atkinson <michael.atkinson@manchester.ac.uk>
2023-01-17 17:07:49 +0000

    Add equality check for Quat

--- defdap/quat.py
+++ defdap/quat.py
@@ -220,6 +220,13 @@ class Quat(object):
     def __str__(self) -> str:
         return self.__repr__()
 
+    def __eq__(self, right: 'Quat') -> bool:
+        return (isinstance(right, type(self)) and 
+            self.quatCoef.tolist() == right.quatCoef.tolist())
+
+    def __hash__(self) -> int:
+        return hash(tuple(self.quatCoef.tolist()))
+
     def _plotIPF(
         self,
         direction: np.ndarray,
