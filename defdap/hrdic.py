3f4c4d130342681aadc711c48bffc13d826eb1f8
Rhys Thomas <rhys.thomas@manchester.ac.uk>
2024-12-08 21:39:07 +0000

    Implement mixin class for hrdic/optical

--- defdap/hrdic.py
+++ defdap/hrdic.py
@@ -30,14 +30,14 @@ import peakutils
 from defdap._accelerated import flood_fill_dic
 from defdap.utils import Datastore, report_progress
 from defdap.file_readers import DICDataLoader, DavisLoader
-from defdap import base
+from defdap import base, mixin
 
 from defdap import defaults
-from defdap.plotting import MapPlot, GrainPlot
+from defdap.plotting import MapPlot
 from defdap.inspector import GrainInspector
 
 
-class Map(base.Map):
+class Map(base.Map, mixin.Map):
     """
     Class to encapsulate DIC map data and useful analysis and plotting
     methods.
@@ -308,139 +308,6 @@ class Map(base.Map):
             for i in np.ndindex(self.data[c].shape[:len(np.shape(self.data[c]))-2]):
                 per = [np.nanpercentile(self.data[c][i], p) for p in percentiles]
                 print(str_format.format(c+''.join([str(t+1) for t in i]), *per))
-
-    def set_crop(self, *, left=None, right=None, top=None, bottom=None,
-                 update_homog_points=False):
-        """Set a crop for the DIC map.
-
-        Parameters
-        ----------
-        left : int
-            Distance to crop from left in pixels (formally `xMin`)
-        right : int
-            Distance to crop from right in pixels (formally `xMax`)
-        top : int
-            Distance to crop from top in pixels (formally `yMin`)
-        bottom : int
-            Distance to crop from bottom in pixels (formally `yMax`)
-        update_homog_points : bool, optional
-            If true, change homologous points to reflect crop.
-
-        """
-        # changes in homog points
-        dx = 0
-        dy = 0
-
-        # update crop distances
-        if left is not None:
-            left = int(left)
-            dx = self.crop_dists[0, 0] - left
-            self.crop_dists[0, 0] = left
-        if right is not None:
-            self.crop_dists[0, 1] = int(right)
-        if top is not None:
-            top = int(top)
-            dy = self.crop_dists[1, 0] - top
-            self.crop_dists[1, 0] = top
-        if bottom is not None:
-            self.crop_dists[1, 1] = int(bottom)
-
-        # update homogo points if required
-        if update_homog_points and (dx != 0 or dy != 0):
-            self.frame.update_homog_points(homog_idx=-1, delta=(dx, dy))
-
-        # set new cropped dimensions
-        x_dim = self.xdim - self.crop_dists[0, 0] - self.crop_dists[0, 1]
-        y_dim = self.ydim - self.crop_dists[1, 0] - self.crop_dists[1, 1]
-        self.shape = (y_dim, x_dim)
-
-    def crop(self, map_data, binning=None):
-        """ Crop given data using crop parameters stored in map
-        i.e. cropped_data = DicMap.crop(DicMap.data_to_crop).
-
-        Parameters
-        ----------
-        map_data : numpy.ndarray
-            Bap data to crop.
-        binning : int
-            True if mapData is binned i.e. binned BSE pattern.
-        """
-        binning = 1 if binning is None else binning
-
-        min_y = int(self.crop_dists[1, 0] * binning)
-        max_y = int((self.ydim - self.crop_dists[1, 1]) * binning)
-
-        min_x = int(self.crop_dists[0, 0] * binning)
-        max_x = int((self.xdim - self.crop_dists[0, 1]) * binning)
-
-        return map_data[..., min_y:max_y, min_x:max_x]
-
-    def link_ebsd_map(self, ebsd_map, transform_type="affine", **kwargs):
-        """Calculates the transformation required to align EBSD dataset to DIC.
-
-        Parameters
-        ----------
-        ebsd_map : defdap.ebsd.Map
-            EBSD map object to link.
-        transform_type : str, optional
-            affine, piecewiseAffine or polynomial.
-        kwargs
-            All arguments are passed to `estimate` method of the transform.
-
-        """
-        self.ebsd_map = ebsd_map
-        kwargs.update({'type': transform_type.lower()})
-        self.experiment.link_frames(self.frame, ebsd_map.frame, kwargs)
-        self.data.add_derivative(
-            self.ebsd_map.data,
-            lambda boundaries: BoundarySet.from_ebsd_boundaries(
-                self, boundaries
-            ),
-            in_props={
-                'type': 'boundaries'
-            }
-        )
-
-    def check_ebsd_linked(self):
-        """Check if an EBSD map has been linked.
-
-        Returns
-        ----------
-        bool
-            Returns True if EBSD map linked.
-
-        Raises
-        ----------
-        Exception
-            If EBSD map not linked.
-
-        """
-        if self.ebsd_map is None:
-            raise Exception("No EBSD map linked.")
-        return True
-
-    def warp_to_dic_frame(self, map_data, **kwargs):
-        """Warps a map to the DIC frame.
-
-        Parameters
-        ----------
-        map_data : numpy.ndarray
-            Data to warp.
-        kwargs
-            All other arguments passed to :func:`defdap.experiment.Experiment.warp_map`.
-
-        Returns
-        ----------
-        numpy.ndarray
-            Map (i.e. EBSD map data) warped to the DIC frame.
-
-        """
-        # Check a EBSD map is linked
-        self.check_ebsd_linked()
-        return self.experiment.warp_image(
-            map_data, self.ebsd_map.frame, self.frame, output_shape=self.shape,
-            **kwargs
-        )
 
     # TODO: fix component stuff
     def generate_threshold_mask(self, mask, dilation=0, preview=True):
@@ -802,38 +669,6 @@ class Grain(base.Grain):
 
         return plot
 
-    @property
-    def ref_ori(self):
-        """Returns average grain orientation.
-
-        Returns
-        -------
-        defdap.quat.Quat
-
-        """
-        return self.ebsd_grain.ref_ori
-
-    @property
-    def slip_traces(self):
-        """Returns list of slip trace angles based on EBSD grain orientation.
-
-        Returns
-        -------
-        list
-
-        """
-        return self.ebsd_grain.slip_traces
-
-    def calc_slip_traces(self, slip_systems=None):
-        """Calculates list of slip trace angles based on EBSD grain orientation.
-
-        Parameters
-        -------
-        slip_systems : defdap.crystal.SlipSystem, optional
-
-        """
-        self.ebsd_grain.calc_slip_traces(slip_systems=slip_systems)
-
     def calc_slip_bands(self, grain_map_data, thres=None, min_dist=None):
         """Use Radon transform to detect slip band angles.
 
@@ -894,33 +729,3 @@ class Grain(base.Grain):
         slip_band_angles = slip_band_angles * np.pi / 180
         return slip_band_angles
 
-
-class BoundarySet(object):
-    def __init__(self, dic_map, points, lines):
-        self.dic_map = dic_map
-        self.points = set(points)
-        self.lines = lines
-
-    @classmethod
-    def from_ebsd_boundaries(cls, dic_map, ebsd_boundaries):
-        if len(ebsd_boundaries.points) == 0:
-            return cls(dic_map, [], [])
-
-        points = dic_map.experiment.warp_points(
-            ebsd_boundaries.image.astype(float),
-            dic_map.ebsd_map.frame, dic_map.frame,
-            output_shape=dic_map.shape
-        )
-        lines = dic_map.experiment.warp_lines(
-            ebsd_boundaries.lines, dic_map.ebsd_map.frame, dic_map.frame
-        )
-        return cls(dic_map, points, lines)
-
-    def _image(self, points):
-        image = np.zeros(self.dic_map.shape, dtype=bool)
-        image[tuple(zip(*points))[::-1]] = True
-        return image
-
-    @property
-    def image(self):
-        return self._image(self.points)
--- defdap/optical.py
+++ defdap/optical.py
@@ -18,13 +18,12 @@ import numpy as np
 from skimage import measure
 
 from defdap.file_readers import load_image
-from defdap import base
+from defdap import base, mixin
 from defdap.utils import Datastore, report_progress
 from defdap import defaults
-from defdap.plotting import MapPlot
 from defdap.inspector import GrainInspector
 
-class Map(base.Map):
+class Map(base.Map, mixin.Map):
     '''
     This class is for import and analysing optical image data
     such as polarised light images and darkfield images to link
@@ -139,139 +138,6 @@ class Map(base.Map):
             return None
 
         return self.optical_scale 
-        
-    def set_crop(self, *, left=None, right=None, top=None, bottom=None,
-                 update_homog_points=False):
-        """Set a crop for the optical map.
-
-        Parameters
-        ----------
-        left : int
-            Distance to crop from left in pixels (formally `xMin`)
-        right : int
-            Distance to crop from right in pixels (formally `xMax`)
-        top : int
-            Distance to crop from top in pixels (formally `yMin`)
-        bottom : int
-            Distance to crop from bottom in pixels (formally `yMax`)
-        update_homog_points : bool, optional
-            If true, change homologous points to reflect crop.
-
-        """
-        # changes in homog points
-        dx = 0
-        dy = 0
-
-        # update crop distances
-        if left is not None:
-            left = int(left)
-            dx = self.crop_dists[0, 0] - left
-            self.crop_dists[0, 0] = left
-        if right is not None:
-            self.crop_dists[0, 1] = int(right)
-        if top is not None:
-            top = int(top)
-            dy = self.crop_dists[1, 0] - top
-            self.crop_dists[1, 0] = top
-        if bottom is not None:
-            self.crop_dists[1, 1] = int(bottom)
-
-        # update homogo points if required
-        if update_homog_points and (dx != 0 or dy != 0):
-            self.frame.update_homog_points(homog_idx=-1, delta=(dx, dy))
-
-        # set new cropped dimensions
-        x_dim = self.xdim - self.crop_dists[0, 0] - self.crop_dists[0, 1]
-        y_dim = self.ydim - self.crop_dists[1, 0] - self.crop_dists[1, 1]
-        self.shape = (y_dim, x_dim)
-
-    def crop(self, map_data, binning=None):
-        """ Crop given data using crop parameters stored in map
-        i.e. cropped_data = DicMap.crop(DicMap.data_to_crop).
-
-        Parameters
-        ----------
-        map_data : numpy.ndarray
-            Bap data to crop.
-        binning : int
-            True if mapData is binned i.e. binned BSE pattern.
-        """
-        binning = 1 if binning is None else binning
-
-        min_y = int(self.crop_dists[1, 0] * binning)
-        max_y = int((self.ydim - self.crop_dists[1, 1]) * binning)
-
-        min_x = int(self.crop_dists[0, 0] * binning)
-        max_x = int((self.xdim - self.crop_dists[0, 1]) * binning)
-
-        return map_data[..., min_y:max_y, min_x:max_x]
-
-    def link_ebsd_map(self, ebsd_map, transform_type="affine", **kwargs):
-        """Calculates the transformation required to align EBSD dataset to optical.
-
-        Parameters
-        ----------
-        ebsd_map : defdap.ebsd.Map
-            EBSD map object to link.
-        transform_type : str, optional
-            affine, piecewiseAffine or polynomial.
-        kwargs
-            All arguments are passed to `estimate` method of the transform.
-
-        """
-        self.ebsd_map = ebsd_map
-        kwargs.update({'type': transform_type.lower()})
-        self.experiment.link_frames(self.frame, ebsd_map.frame, kwargs)
-        self.data.add_derivative(
-            self.ebsd_map.data,
-            lambda boundaries: BoundarySet.from_ebsd_boundaries(
-                self, boundaries
-            ),
-            in_props={
-                'type': 'boundaries'
-            }
-        )
-
-    def check_ebsd_linked(self):
-        """Check if an EBSD map has been linked.
-
-        Returns
-        ----------
-        bool
-            Returns True if EBSD map linked.
-
-        Raises
-        ----------
-        Exception
-            If EBSD map not linked.
-
-        """
-        if self.ebsd_map is None:
-            raise Exception("No EBSD map linked.")
-        return True
-
-    def warp_to_dic_frame(self, map_data, **kwargs):
-        """Warps a map to the optical frame.
-
-        Parameters
-        ----------
-        map_data : numpy.ndarray
-            Data to warp.
-        kwargs
-            All other arguments passed to :func:`defdap.experiment.Experiment.warp_map`.
-
-        Returns
-        ----------
-        numpy.ndarray
-            Map (i.e. EBSD map data) warped to the optical frame.
-
-        """
-        # Check a EBSD map is linked
-        self.check_ebsd_linked()
-        return self.experiment.warp_image(
-            map_data, self.ebsd_map.frame, self.frame, output_shape=self.shape,
-            **kwargs
-        )
 
     @report_progress("finding grains")
     def find_grains(self, algorithm=None, min_grain_size=10):
@@ -429,65 +295,3 @@ class Grain(base.Grain):
         plot = self.plot_grain_data(grain_data=self.data.image, **plot_params)
 
         return plot
-        
-    @property
-    def ref_ori(self):
-        """Returns average grain orientation.
-
-        Returns
-        -------
-        defdap.quat.Quat
-
-        """
-        return self.ebsd_grain.ref_ori
-
-    @property
-    def slip_traces(self):
-        """Returns list of slip trace angles based on EBSD grain orientation.
-
-        Returns
-        -------
-        list
-
-        """
-        return self.ebsd_grain.slip_traces
-
-    def calc_slip_traces(self, slip_systems=None):
-        """Calculates list of slip trace angles based on EBSD grain orientation.
-
-        Parameters
-        -------
-        slip_systems : defdap.crystal.SlipSystem, optional
-
-        """
-        self.ebsd_grain.calc_slip_traces(slip_systems=slip_systems)
-
-class BoundarySet(object):
-    def __init__(self, dic_map, points, lines):
-        self.dic_map = dic_map
-        self.points = set(points)
-        self.lines = lines
-
-    @classmethod
-    def from_ebsd_boundaries(cls, dic_map, ebsd_boundaries):
-        if len(ebsd_boundaries.points) == 0:
-            return cls(dic_map, [], [])
-
-        points = dic_map.experiment.warp_points(
-            ebsd_boundaries.image.astype(float),
-            dic_map.ebsd_map.frame, dic_map.frame,
-            output_shape=dic_map.shape
-        )
-        lines = dic_map.experiment.warp_lines(
-            ebsd_boundaries.lines, dic_map.ebsd_map.frame, dic_map.frame
-        )
-        return cls(dic_map, points, lines)
-
-    def _image(self, points):
-        image = np.zeros(self.dic_map.shape, dtype=bool)
-        image[tuple(zip(*points))[::-1]] = True
-        return image
-
-    @property
-    def image(self):
-        return self._image(self.points)
